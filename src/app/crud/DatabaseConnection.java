5ff816459887e5f47430cb08135df789af008545
birugh <notblue.https@gmail.com>
2025-02-04 21:36:36 +0700

    Membuat logika dari DatabaseConnection.java

--- src/app/crud/DatabaseConnection.java
+++ src/app/crud/DatabaseConnection.java
@@ -1,11 +1,24 @@
 package app.crud;
 
-import java.util.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /*
  *  @author biru
  */
 
 public class DatabaseConnection {
+    private static final String url = "jdbc:mysql://localhost:3306/nama_database";
+    private static final String user = "root";
+    private static final String password = "";
 
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Koneksi ke database gagal: " + e.getMessage());
+            return null;
+        }
+    }
 }
